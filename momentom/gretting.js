b104c32fbcf8b09c6443934f1300293aac438641
dbys34 <55341412+dbys34@users.noreply.github.com>
2020-12-28 14:03:54 +0900

    Add files via upload

--- momentom/gretting.js
+++ momentom/gretting.js
@@ -24,11 +24,17 @@ function askForName(){
 }
 
 function paintGreeting(text) {
+  const link = document.createElement("a");
+  link.href = "popup.html";
+  link.target ="_blank"
+  link.innerText = "알람 미리보기";
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
   greeting.innerText = `안녕하세요, ${text} 님,\n 
   설정한 시간이 되면 자동으로 깜빡이는 팝업창이 뜹니다. 그동안 다른일 보세요. \n
-  새로고침하면 입력한 시간은 날라가니 주의해주세요`;
+  새로고침하면 입력한 시간은 날라가니 주의해주세요.  `;
+  greeting.appendChild(link);
+
 }
 
 function loadName() {
