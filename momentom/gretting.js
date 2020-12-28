2ab5b312fb4a440f855a55c5a792d6dae2d4c474
dbys34 <55341412+dbys34@users.noreply.github.com>
2020-12-28 10:15:02 +0900

    Add files via upload

--- momentom/gretting.js
+++ momentom/gretting.js
@@ -26,9 +26,8 @@ function askForName(){
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `안녕하세요, ${text} 님, 당직 타이머 설정법을 안내 해드리겠습니다. \n 
-  깨울 사람을 입력하고, 시간을 설정해주세요. \n
-  설정한 시간이 되면 자동으로 팝업창이 뜹니다. 그동안 휴머니스트 즐감하세요. \n
+  greeting.innerText = `안녕하세요, ${text} 님,\n 
+  설정한 시간이 되면 자동으로 깜빡이는 팝업창이 뜹니다. 그동안 다른일 보세요. \n
   새로고침하면 입력한 시간은 날라가니 주의해주세요`;
 }
 
--- momentom/todo.js
+++ momentom/todo.js
@@ -1,9 +1,8 @@
 const toDoform = document.querySelector(".js-toDoForm"),
     toDoInput = toDoform.querySelector("input"),
     toDoList = document.querySelector(".js-toDoList");
-
 const TODOS_LS = 'toDos';
-
+let idNumbers = 1;
 let toDos = [];
 
 
@@ -11,47 +10,42 @@ function deleteToDo(event){
     const btn = event.target;
     const li = btn.parentNode;
     toDoList.removeChild(li) /*html 에서 지우기*/
-
     /*필터링(클릭한거 빼고 선택되도록)*/
     const cleanToDos = toDos.filter(function(toDo) {
         return toDo.id !== parseInt(li.id) /*파라미터에 대한 조건*/;
     });
-
     toDos = cleanToDos; /* toDos를 새로 정의*/
     saveToDos(); /*새로 정의해서 바뀐 toDos를 local storage에 저장*/
-
-   /*local storage 에서 지우기*/
 };
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
-let idNumbers = 1;
-
 function paintToDo(text){
     const li = document.createElement("li");
     const span = document.createElement("span");
     const delBtn = document.createElement("button");
-
     const input = document.createElement("input");
-
     const newId =  idNumbers;
+
     idNumbers += 1;
     delBtn.innerText = "X";
     delBtn.addEventListener("click", deleteToDo);
+    delBtn.className = "btn btn-secondary";
+
     span.innerText = text;
     input.type = 'time';
     input.min = '00:00';
     input.max = '23:59';
     input.className = 'WakeUpTime';
-
     input.id = newId;
 
     li.appendChild(input);
     li.appendChild(span);
     li.appendChild(delBtn);
     li.id = newId;
+    li.className = "list-group-item grid";
     toDoList.appendChild(li);
     const toDoObj = {
         text: text,
@@ -67,6 +61,10 @@ function handleSubmit(event){
     paintToDo(currentValue);
     toDoInput.value = "";
 }
+
+
+
+
 
 
 function loadToDos(){
